1ff7c70080bd8b233791e90d6316b9ca78b32702
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2024-08-11 15:41:33 -0600

    Variables

--- js/01.js
+++ js/01.js
@@ -1,1 +1,37 @@
 // Variables
+
+// Forma deprecada de crear variables en JS
+var producto = 'Audifonos Gamer';  // Iniciar variable y asignarle valor
+
+var disponible;  // Iniciar variable pero sin valor
+
+//! JavaScript es de tipado dinámico
+// Guarda el tipo de dato en el valor que tiene la variable y no en la variable como en otros lenguajes (como en C)
+
+// producto = true;  // Reasignando el valor de la variable
+
+disponible = true;
+
+var producto1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+//! No se permite nombrar una variable con numeros al inicio
+
+// var 1disponible;
+// var -disponible = true;
+
+//? Si pueden empezar con guion bajo
+
+var _disponible;
+
+//* Estilos para las variables
+
+var nombre_producto = "Monitor HD";  // underscore
+var nombreProducto = "Monitor HD"  // Camelcase
+var NombreProducto = 'Monitor HD';  // Pascal Case
+var nombreproducto = 'Monitor HD';
+
+//? Las variables son Case Sensitive
+
+console.log(PRODUCTO);
