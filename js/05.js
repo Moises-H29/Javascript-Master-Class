5806d8c4650112866700583a2dfc4cd09491c22b
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2024-08-11 22:43:13 -0600

    Numbers en JS

---
+++ js/05.js
@@ -0,0 +1,21 @@
+// Números
+
+const nombre = "Moises Hernandez";
+const numero1 = 100;
+const numero2 = 200;
+const numero3 = 200.20;
+const numero4 = .1020;
+const numero5 = -5;
+
+
+
+console.log(numero1);
+console.log(numero2);
+console.log(numero3);
+
+
+console.log(numero1 + numero2);
+console.log(numero2 - numero1);
+console.log(numero1 * numero2);
+console.log(numero2 / numero1);
+console.log(numero2 % numero1);
