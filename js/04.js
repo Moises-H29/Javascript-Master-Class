ed5e9a2b5b435287da0bb9c0320f366c36e06038
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2024-08-11 19:48:49 -0600

    Introduccion a los Strings en JS

---
+++ js/04.js
@@ -0,0 +1,12 @@
+// String o Cadenas de Texto
+const producto = 'Monitor de 20"';
+
+// Usando el constructor de String
+const producto2 = String('Monitor de 30 pulgadas');
+
+// Usando new para construir objetos
+const producto3 = new String('Monitor de 50 pulgadas');
+
+console.log(producto);
+console.log(producto2);
+console.log(typeof producto3);
