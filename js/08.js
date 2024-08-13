23bb95ee93af058abad82f8ce977b35698c3a964
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2024-08-13 12:06:40 -0600

    String Literals y Concatenacion

---
+++ js/08.js
@@ -0,0 +1,8 @@
+const nombre = 'Moises';
+const email = 'correo@correo.com';
+
+// Concatenación
+console.log("Nombre Cliente: " + nombre + " Email: " + email);
+
+// Template Strings - String Literals
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);
