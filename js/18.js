58481ce1000e384860fefe3da935ab43c3996a41
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2024-09-12 15:48:19 -0600

    Argumentos y parámetros de una función

---
+++ js/18.js
@@ -0,0 +1,16 @@
+
+function sumar(numero1 = 0, numero2 = 0) {  //* Parámetros de la función con el valor 0 por default
+    console.log(numero1 + numero2);
+}
+sumar(null,2);  //* Argumentos o valores asignados
+sumar(2, 5);
+sumar(3, 6);
+sumar(4, -1);
+sumar(2);
+sumar();
+
+
+const sumar2 = function(n1, n2) {
+    console.log(3 + 3);
+}
+sumar2();
