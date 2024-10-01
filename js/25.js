2e6e49ccd8cdde13cfeddd8a30cf920cfe1fe54a
Moises-H29 <ironmoyher@outlook.com>
2024-09-30 23:54:43 -0600

    ForEach y Map

---
+++ js/25.js
@@ -0,0 +1,18 @@
+const carrito = [
+    { nombre: 'Monitor 20 Pulgadas', precio: 500 },
+    { nombre: 'Television 50 Pulgadas', precio: 700 },
+    { nombre: 'Tablet', precio: 300 },
+    { nombre: 'Audifonos', precio: 200 },
+    { nombre: 'Teclado', precio: 50 },
+    { nombre: 'Celular', precio: 500 },
+    { nombre: 'Bocinas', precio: 300 },
+    { nombre: 'Laptop', precio: 800 }
+];
+
+// ForEach
+
+carrito.forEach( function() {
+    console.log("Una vez por cada elemento");
+} );
+
+// map
