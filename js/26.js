6715ecf75e26f80e5abb0d551bea7f1f49fb9f83
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2024-10-01 23:51:40 -0600

    Uso de this

---
+++ js/26.js
@@ -0,0 +1,37 @@
+//* This
+
+// Object literal
+const reservacion = {
+    nombre: 'Moises',
+    apellido: 'Hernandez',
+    total: 5000,
+    pagado: false,
+    informacion: function() {
+        console.log(this);  // Imprime contexto del this (objeto reservacion)
+        console.log(`El Cliente ${this.nombre} reservó y su cantidad a pagar es de ${this.total}`);
+    }
+}
+
+//! No podemos utilizar arrow functions en los métodos de nuestro objeto porque hacen referencia a la ventana global
+const reservacion2 = {
+    nombre: 'Moises',
+    apellido: 'Hernandez',
+    total: 5000,
+    pagado: false,
+    informacion: () => {
+        console.log(this);  // Imprime contexto del this (ventana global)
+        console.log(`El Cliente ${this.nombre} reservó y su cantidad a pagar es de ${this.total}`);
+    }
+}
+
+const prueba = {
+    nombrePersona: 'Juan',
+    imprime: () => {
+        console.log(`${this.nombrePersona}`);
+    }
+}
+
+reservacion.informacion();
+reservacion2.informacion();
+
+prueba.imprime();
