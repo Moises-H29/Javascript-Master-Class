8f049d9d22ceb9a0b213966c7f0df925ae3e2460
Moises Hernandez Pacheco <ironmoyher@outlook.com>
2024-10-22 12:55:28 -0600

    Object Constructor

--- js/27.js
+++ js/27.js
@@ -7,3 +7,19 @@ const producto = {
 }
 
 //* Object Constructor
+
+function Producto(nombre, precio, disponible) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.disponible = disponible;
+}
+
+const producto2 = new Producto('Monitor de 49"', 800, true);
+const producto3 = new Producto('Laptop', 300, false);
+const producto4 = new Producto('Audifonos', 50, true);
+const producto5 = new Producto('Celular', 200, true);
+
+console.log(producto2);
+console.log(producto3);
+console.log(producto4);
+console.log(producto5);
