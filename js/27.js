115905f62b5b7813dfa3860829e6c9a48ac78d1e
Moises-H29 <ironmoyher@outlook.com>
2024-10-24 18:22:14 -0600

    Classes y object constructor

--- js/27.js
+++ js/27.js
@@ -29,8 +29,8 @@ Producto.prototype.formatearProducto = function() {
     return `El producto ${this.nombre} tiene un precio de $ ${this.precio}`
 }
 
-const producto2 = new Producto('Monitor de 49"', 800, true);
-const producto3 = new Producto('Laptop', 300, false);
+        const producto2 = new Producto('Monitor de 49"', 800, true);
+        const producto3 = new Producto('Laptop', 300, false);
 const producto4 = new Producto('Audifonos', 50, true);
 const producto5 = new Producto('Celular', 200, true);
 
@@ -40,7 +40,7 @@ console.log(cliente);
 
 //! Intentando formatear nuestro objeto SIN usar prototype nos hará escribir muchas funciones únicas para cada objeto
 function formatearProducto(producto) {
-    return `El producto ${producto.nombre} tiene un precio de $ ${producto.precio}`
+    return `El producto ${producto.nombre} tiene un precio de $ ${producto.precio}`;
 }
 console.log(producto2);
 console.log(producto2.formatearProducto());
--- js/28.js
+++ js/28.js
@@ -1,7 +1,23 @@
 // Classes
 
 class Producto {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
 
+    formatearProducto(producto) {
+        return `El producto ${this.nombre} tiene un precio de $ ${this.precio}`;
+    }
+    precioProducto(producto) {
+        return this.precio;
+    }
 }
 
-const producto = new Producto();
+const producto2 = new Producto('Monitor de 49"', 800, true);
+const producto3 = new Producto('Laptop', 300, false);
+
+console.log(producto2);
+console.log(producto3);
+console.log(producto2.formatearProducto());
+console.log(producto2.precioProducto());
