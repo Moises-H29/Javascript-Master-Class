32161ba6e4396db95c6b870aa67505176da585b6
Moises-H29 <ironmoyher@outlook.com>
2024-10-26 23:47:50 -0600

    Notification API y Promises

---
+++ js/31.js
@@ -0,0 +1,12 @@
+const boton = document.querySelector('#boton');
+boton.addEventListener('click', function() {
+    Notification.requestPermission()
+        .then(resultado => console.log(`El resultado es ${resultado}`))
+});
+
+if (Notification.permission == 'granted') {
+    new Notification('Esta es una notificación', {
+        icon: 'img/ccj.png',
+        body: 'Código con Juan, los mejores tutoriales'
+    })
+}
