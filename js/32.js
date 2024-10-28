13bc18464478f9967dc2f4ddb67d3c27c9a6adbb
Moises-H29 <ironmoyher@outlook.com>
2024-10-27 23:33:48 -0600

    Optimizando Async/Await con Promise.all

--- js/32.js
+++ js/32.js
@@ -11,11 +11,28 @@ function descargarNuevosClientes() {
     } )
 }
 
+function descargarUltimosPedidos() {
+    return new Promise( resolve => {
+        console.log("Descargando pedidos... Espere...");
+
+        setTimeout(() => {
+            resolve("Los pedidos fueron descargados")
+        }, 3000);
+
+    } )
+}
+
 async function app() {
     try {
-        const resultado = await descargarNuevosClientes();  // El código de abajo se ejecuta hasta que la función haya terminado su ejecución
-        console.log("Este codigo si se bloquea");
-        console.log(resultado);
+        // const clientes = await descargarNuevosClientes();  // El código de abajo se ejecuta hasta que la función haya terminado su ejecución
+        // const pedidos = await descargarUltimosPedidos();  
+        // console.log(clientes);
+        // console.log(pedidos);
+        
+        //? Optimizando un llamado Async / Await (con un arreglo de Promises: Promise.all)
+        const resultado = await Promise.all([ descargarNuevosClientes(), descargarUltimosPedidos() ]);  //! Ahora empiezan a ejecutarse a la vez
+        console.log(resultado[0]);
+        console.log(resultado[1]);
     } catch (error) {
         console.log(error);
     }
